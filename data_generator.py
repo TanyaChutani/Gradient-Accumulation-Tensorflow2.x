948fd18c280589ddcd80c5b7441b4d1992b3043f
Tanya <tanyach1997@gmail.com>
2022-03-09 03:41:35 +0530

    cifar10 read

--- data_generator.py
+++ data_generator.py
@@ -1,54 +1,47 @@
+from numpy.core.fromnumeric import shape
 import os
 import numpy as np
 import tensorflow as tf
 
 class DataGenerator:
-    def __init__(
-        self,
-        mode,
-        data_path,
-        batch_size,
-        resize_dim,
-        shuffle=True,
-        n_channels=3,
-    ):
-        self.mode = mode
-        self.data_path = data_path
-        self.batch_size = batch_size
-        self.resize_dim = resize_dim
-        self.shuffle = shuffle
-        self.n_channels = n_channels
-        self.data = os.listdir((os.path.join(self.data_path, self.mode)))
-        self.on_epoch_end()
+  def __init__(
+      self,
+      dataset_name,
+      batch_size,
+      resize_dim,
+      mode,
+      shuffle=True,
+      n_channels=3,
+  ):
+    self.mode = mode
+    self.dataset_name = dataset_name
+    self.batch_size = batch_size
+    self.resize_dim = resize_dim
+    self.shuffle = shuffle
+    self.n_channels = n_channels
 
-    def __len__(self):
-        return int(np.ceil(len(self.data) / self.batch_size))
+  def __len__(self):
+      return int(np.ceil((self.images).shape[0] / self.batch_size))
 
-    def __call__(self):
-        for i in self.index:
-            x, y = self.load(os.path.join(self.data_path, self.mode, self.data[i]))
-            yield x, y
+  def __call__(self):
+    data_name = self.dataset_name 
+    (x_train,y_train),(x_test,y_test) = tf.keras.datasets.data_name.load_data()
+    self.images, self.labels = (x_train,y_train) if self.mode == "train" else (x_test,y_test)
 
-    def on_epoch_end(self):
-        self.index = np.arange(len(self.data))
-        if self.shuffle == True:
-            np.random.shuffle(self.index)
+    for i in self.index:
+      print(self.index)
+      x, y = self.load(self.preprocess(self.images[i]), self.labels[i])
+      yield x, y
 
-    def preprocess(self, image):
-        image = tf.image.resize(
-            image, size=self.resize_dim, method=tf.image.ResizeMethod.BILINEAR
-        )
+  def on_epoch_end(self):
+      self.index = np.arange((self.images).shape[0])
+      if self.shuffle == True:
+          np.random.shuffle(self.index)
 
-        image = image / 255.0
-        image = tf.cast(image, tf.float32)
-        return image
-
-    def load(self, image_path):
-        image = tf.io.read_file(image_path)
-        image = tf.image.decode_png(image, channels=self.n_channels)
-        image.set_shape([None, None, self.n_channels])
-
-        image = self.preprocess(image)
-
-        return image
-
+  def preprocess(self, image):
+      image = tf.image.resize(
+          image, size=self.resize_dim, method=tf.image.ResizeMethod.BILINEAR
+      )
+      image = image / 255.0
+      image = tf.cast(image, tf.float32)
+      return image
