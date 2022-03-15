fc30559695a193c29118b37478e17bbdf29c1da2
Tanya <tanyach1997@gmail.com>
2022-03-16 01:53:28 +0530

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,47 +1,45 @@
-from numpy.core.fromnumeric import shape
-import os
-import numpy as np
 import tensorflow as tf
 
+
 class DataGenerator:
-  def __init__(
-      self,
-      dataset_name,
-      batch_size,
-      resize_dim,
-      mode,
-      shuffle=True,
-      n_channels=3,
-  ):
-    self.mode = mode
-    self.dataset_name = dataset_name
-    self.batch_size = batch_size
-    self.resize_dim = resize_dim
-    self.shuffle = shuffle
-    self.n_channels = n_channels
+    def __init__(
+        self,
+        mode,
+        batch_size,
+        resize_dim,
+        shuffle=True,
+        n_channels=3,
+    ):
+        self.mode = mode
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        self.resize_dim = resize_dim
+        self.n_channels = n_channels
 
-  def __len__(self):
-      return int(np.ceil((self.images).shape[0] / self.batch_size))
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-  def __call__(self):
-    data_name = self.dataset_name 
-    (x_train,y_train),(x_test,y_test) = tf.keras.datasets.data_name.load_data()
-    self.images, self.labels = (x_train,y_train) if self.mode == "train" else (x_test,y_test)
+        self.images, self.labels = (
+            (x_train, y_train) if self.mode == "train" else (x_test, y_test)
+        )
 
-    for i in self.index:
-      print(self.index)
-      x, y = self.load(self.preprocess(self.images[i]), self.labels[i])
-      yield x, y
+        self.index = tf.range((self.images).shape[0])
 
-  def on_epoch_end(self):
-      self.index = np.arange((self.images).shape[0])
-      if self.shuffle == True:
-          np.random.shuffle(self.index)
+    def __len__(self):
+        return int(tf.math.ceil((self.images).shape[0] / self.batch_size))
 
-  def preprocess(self, image):
-      image = tf.image.resize(
-          image, size=self.resize_dim, method=tf.image.ResizeMethod.BILINEAR
-      )
-      image = image / 255.0
-      image = tf.cast(image, tf.float32)
-      return image
+    def __call__(self):
+        for i in self.index:
+            x, y = self.preprocess(self.images[i]), self.labels[i]
+            yield x, y
+
+    def on_epoch_end(self):
+        if self.shuffle == True:
+            tf.random.shuffle(self.index)
+
+    def preprocess(self, image):
+        image = tf.image.resize(
+            image, size=self.resize_dim, method=tf.image.ResizeMethod.BILINEAR
+        )
+        image = image / 255.0
+        image = tf.cast(image, tf.float32)
+        return image
