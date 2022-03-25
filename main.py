17d49b448c5f3cb17c58b85313c010cded2587e3
Tanya <tanyach1997@gmail.com>
2022-03-25 22:34:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import tensorflow as tf
 import argparse
 
+
+model = Classification(5)
+model.build((1,32,32,3))
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate),
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(),
+    metric=tf.keras.metrics.SparseCategoricalAccuracy(),
+)
+model.fit(
+    train_dataset,
+    steps_per_epoch=train_gen.__len__(),
+    epochs=2,
+    callbacks=callbacks,
+)
+
